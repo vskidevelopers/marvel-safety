efe6e552712594dc404c435f237b38d5b61da2a9
v.skidevelopers <v.skidevelopers@gmail.com>
2026-01-12 13:39:23 +0300

    fixed vercel redirect

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,24 +1,23 @@
 "use client";
 
 import { ReactNode, useEffect, useState } from "react";
-import { ConvexReactClient } from "convex/react";
-import { useAuth } from "@clerk/nextjs";
+// import { ConvexReactClient } from "convex/react";
+// import { useAuth } from "@clerk/nextjs";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
     children,
 }: {
     children: ReactNode;
 }) {
-    const { getToken, isLoaded, userId } = useAuth();
-    const [ready, setReady] = useState(false);
+    // const { getToken, isLoaded, userId } = useAuth();
+    const [ready, setReady] = useState(true);
 
     // useEffect(() => {
     //     const setupAuth = async () => {
     //         if (!isLoaded) return;
 
-<<<<<<< HEAD
     //         try {
     //             if (userId) {
     //                 const token = await getToken({ template: "convex" });
@@ -30,19 +29,6 @@ export default function ConvexClientProvider({
     //             console.error("Auth setup failed", error);
     //             convex.setAuth(undefined);
     //         }
-=======
-            try {
-                if (userId) {
-                    const token = await getToken({ template: "convex" });
-                    convex.setAuth(token || "");
-                } else {
-                    convex.setAuth("");
-                }
-            } catch (error) {
-                console.error("Auth setup failed", error);
-                convex.setAuth("");
-            }
->>>>>>> 4b6b1a0a6586fc76549fc9a61ac73e4399ab2c7e
 
     //         setReady(true);
     //     };
