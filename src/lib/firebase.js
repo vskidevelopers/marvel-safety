4e73ccfee5e0348fa98cf38791131c64ed24d058
v.skidevelopers <v.skidevelopers@gmail.com>
2026-01-27 22:03:11 +0300

    used local env for env vars

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -23,13 +23,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCoWSvVJNz1GzLUQOLuhHnZBys6AHi6Y9Q",
-  authDomain: "marvelsafety-cb495.firebaseapp.com",
-  projectId: "marvelsafety-cb495",
-  storageBucket: "marvelsafety-cb495.firebasestorage.app",
-  messagingSenderId: "706780571294",
-  appId: "1:706780571294:web:d08599598fca29cc0afd8f",
-  measurementId: "G-WCLQSBEMKB",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
